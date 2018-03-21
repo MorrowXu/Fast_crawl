9dd3b3bcd72ea27f4064569148f44c12d36e6a34
Morrow <464580843@qq.com>
2018-03-21 12:41:50 +0800

    2018-3-21 orm

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -4,25 +4,24 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from tutorial import sql_setting
 from tutorial.mysql_orm import add_data # 调用orm映射
 
 class TutorialPipeline(object):
 
     def process_item(self, item, spider):
         data = {}
+        f = add_data()
         if spider.name == 'baike_key':
-            sql = sql_setting.Sql()
             data['url'] = item['url']
             data['title'] = item['title']
             data['para'] = item['para']
-            sql.sql_cennector(data)
+            f.baike_conn(data)
 
         else:
             data['url'] = item['page_url']
             data['title'] = item['page_title']
             data['content'] = item['page_content']
             data['create_time'] = item['page_create_time']
-            add_data(data)
+            f.jobbole_conn(data)
 
         return item
